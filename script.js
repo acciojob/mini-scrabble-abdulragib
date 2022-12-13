596c58f2a8f846cf504e87f5133345f439bb3cb4
Abdul Ragib <abdulragib36@gmail.com>
2022-12-13 19:16:18 +0530

    Solution 1.0

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let displayText=document.getElementById('evaluatedText')
+let displayCount=document.getElementById('letterCount')
+
+evaluatedText.onkeyup= () =>{
+    displayCount.innerText = displayText.value.length
+}
+  
